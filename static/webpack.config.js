58f45f3582dc45259c6fabf49e38699fa4ea5282
JetMuffin <564936642@qq.com>
2016-03-03 12:26:11 +0800

    try to rebuild frontend part with angularjs

--- static/webpack.config.js
+++ static/webpack.config.js
@@ -1,43 +1,109 @@
-var path = require('path');
-var ExtractTextPlugin = require("extract-text-webpack-plugin");
-module.exports = {
-    entry: './src/app',
-    output: {
-        path: path.join(__dirname, './dist'),
-        filename: 'build.js',
-        publicPath: '/dist/'
-    },
+'use strict';
 
-    module: {
-        // 加载器
-        loaders: [
-            { test: /\.vue$/, loader: 'vue' },
-            { test: /\.js$/, loader: 'babel', exclude: /node_modules/ },
-            { test: /\.css$/, loader: 'style!css!autoprefixer'},
-            { test: /\.scss$/, loader: ExtractTextPlugin.extract("style-loader", 'css-loader!sass-loader')},
-            { test: /\.css$/, loader: ExtractTextPlugin.extract("style-loader", "css-loader")},
-            { test: /\.(png|jpg|gif)$/, loader: 'url-loader'},
-            { test: /\.(html|tpl)$/, loader: 'html-loader' },
-            { test: /\.json$/, loader: 'json' },
-        ]
+// Modules
+var webpack = require('webpack');
+var autoprefixer = require('autoprefixer');
+var HtmlWebpackPlugin = require('html-webpack-plugin');
+var ExtractTextPlugin = require('extract-text-webpack-plugin');
+var CopyWebpackPlugin = require('copy-webpack-plugin');
+
+var ENV = process.env.npm_lifecycle_event;
+var isTest = ENV === 'test' || ENV === 'test-watch';
+var isProd = ENV === 'build';
+
+module.exports = function makeWebpackConfig () {
+  var config = {};
+
+  config.entry = isTest ? {} : {
+    app: './src/app/app.js'
+  };
+
+  config.output = isTest ? {} : {
+    path: __dirname + '/dist',
+    publicPath: isProd ? '/' : 'http://localhost:8080/',
+    filename: isProd ? '[name].[hash].js' : '[name].bundle.js',
+    chunkFilename: isProd ? '[name].[hash].js' : '[name].bundle.js'
+  };
+
+  if (isTest) {
+    config.devtool = 'inline-source-map';
+  } else if (isProd) {
+    config.devtool = 'source-map';
+  } else {
+    config.devtool = 'eval-source-map';
+  }
+
+  // Initialize module
+  config.module = {
+    preLoaders: [],
+    loaders: [{
+      test: /\.js$/,
+      loader: 'babel',
+      exclude: /node_modules/
+    }, {
+      test: /\.css$/,
+      loader: isTest ? 'null' : ExtractTextPlugin.extract('style', 'css?sourceMap!postcss')
+    }, { 
+      test: /\.scss$/, 
+      loader: ExtractTextPlugin.extract("style-loader", 'css-loader!sass-loader')
     },
-    vue: {
-        css: ExtractTextPlugin.extract("css"),
-        sass: ExtractTextPlugin.extract("css!sass-loader")
-    },
-    babel: {
-        presets: ['es2015'],
-        plugins: ['transform-runtime']
-    },
-    resolve: {
-        extensions: ['', '.js', '.vue'],
-        alias: {
-            filter: path.join(__dirname, './src/filters'),
-            components: path.join(__dirname, './src/components')
-        }
-    },
-    plugins: [
-        new ExtractTextPlugin("styles.css")
-    ],
-    devtool: '#source-map'
-};
+    {
+      test: /\.(png|jpg|jpeg|gif|svg|woff|woff2|ttf|eot)$/,
+      loader: 'file'
+    }, {
+      test: /\.html$/,
+      loader: 'raw'
+    }]
+  };
+
+
+  if (isTest) {
+    config.module.preLoaders.push({
+      test: /\.js$/,
+      exclude: [
+        /node_modules/,
+        /\.spec\.js$/
+      ],
+      loader: 'isparta-instrumenter'
+    })
+  }
+
+  config.postcss = [
+    autoprefixer({
+      browsers: ['last 2 version']
+    })
+  ];
+
+  config.plugins = [];
+
+  // Skip rendering index.html in test mode
+  if (!isTest) {
+    config.plugins.push(
+      new HtmlWebpackPlugin({
+        template: './src/public/index.html',
+        inject: 'body'
+      }),
+      new ExtractTextPlugin('[name].[hash].css', {disable: !isProd})
+    )
+  }
+
+  // Add build specific plugins
+  if (isProd) {
+    config.plugins.push(
+      new webpack.NoErrorsPlugin(),
+      new webpack.optimize.DedupePlugin(),
+      new webpack.optimize.UglifyJsPlugin(),
+
+      new CopyWebpackPlugin([{
+        from: __dirname + '/src/public'
+      }])
+    )
+  }
+
+  config.devServer = {
+    contentBase: './src/public',
+    stats: 'minimal'
+  };
+
+  return config;
+}();
