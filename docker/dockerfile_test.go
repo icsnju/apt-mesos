7b839a0e42b0bdd08e9186de925ca11c67950cd7
JetMuffin <564936642@qq.com>
2016-04-13 09:28:13 +0800

    if core cannot connect to agent, sleep for a while. fixed #25

--- docker/dockerfile_test.go
+++ docker/dockerfile_test.go
@@ -5,16 +5,21 @@ import (
 	"testing"
 
 	"github.com/icsnju/apt-mesos/docker"
+	"github.com/icsnju/apt-mesos/mesosproto"
 	. "github.com/smartystreets/goconvey/convey"
 )
 
 func TestParse(t *testing.T) {
 	Convey("parse dockerfile", t, func() {
-		dockerfile := docker.NewDockerfile("../examples/Dockerfile", "icsnju")
+		dockerfile := docker.NewDockerfile("../examples/docker_context/Dockerfile", "icsnju")
 		out := dockerfile.Build()
 		fmt.Println(out)
 		So(dockerfile, ShouldNotBeNil)
 		So(dockerfile.HasLocalSources(), ShouldBeTrue)
 		dockerfile.BuildContext()
 	})
+}
+
+func TestPrepareContext(t *testing.T) {
+	var taskInfo mesosproto.TaskInfo
 }
