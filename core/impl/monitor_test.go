de02fcf44c742f2d96790572560b2ba0c95205c6
JetMuffin <564936642@qq.com>
2016-04-20 09:53:52 +0800

    test: remove fetch data test (used to fetch data at 192.168.33.10

--- core/impl/monitor_test.go
+++ core/impl/monitor_test.go
@@ -1,17 +1,1 @@
 package impl
-
-import (
-	"fmt"
-	"testing"
-
-	. "github.com/smartystreets/goconvey/convey"
-)
-
-func TestFetchMetricData(t *testing.T) {
-	Convey("fetch metric data", t, func() {
-		data, err := c.FetchMetricData()
-		So(err, ShouldBeNil)
-		So(data, ShouldNotBeNil)
-		fmt.Println(data)
-	})
-}
