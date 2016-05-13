255e9dc413f2d05f60c433bfe8928562fcf80562
JetMuffin <564936642@qq.com>
2016-05-13 17:16:02 +0800

    add lost script files

---
+++ scripts/test-build.sh
@@ -0,0 +1,1 @@
+curl -X POST  -H 'content-type: application/json'  -d @nodejs.json http://192.168.33.1:3030/api/jobs
---
+++ scripts/test-scheduler.sh
@@ -0,0 +1,3 @@
+curl -X POST  -H 'content-type: application/json'  -d @job1.json http://192.168.33.1:3030/api/jobs
+curl -X POST  -H 'content-type: application/json'  -d @job2.json http://192.168.33.1:3030/api/jobs
+curl -X POST  -H 'content-type: application/json'  -d @job3.json http://192.168.33.1:3030/api/jobs
